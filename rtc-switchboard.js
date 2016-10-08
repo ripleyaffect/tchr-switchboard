3459fa593f3dc07c44b43b4a2fdab59af7951990
Thomas Peterson <t.ripley.peterson@gmail.com>
2016-10-08 17:34:03 -0400

    Bring over code, add events

---
+++ rtc-switchboard.js
@@ -0,0 +1,42 @@
+/* jshint node: true */
+'use strict';
+
+module.exports = function(server, opts) {
+  var WebSocketServer = require('ws').Server;
+  var wss = new WebSocketServer({ server: server });
+  var board = require('./rtc-switch')();
+  var connections = [];
+
+  wss.on('connection', function connection(ws) {
+    var peer = board.connect();
+
+    // add the socket to the connection list
+    connections.push(ws);
+
+    ws.on('message', peer.process);
+    peer.on('data', function(data) {
+      if (ws.readyState === 1) {
+        ws.send(data);
+      }
+    });
+
+    ws.on('close', function() {
+      // trigger the peer leave
+      peer.leave();
+
+      // splice out the connection
+      connections = connections.filter(function(conn) {
+        return conn !== ws;
+      });
+    });
+  });
+
+  // add a reset helper
+  board.reset = function() {
+    connections.splice(0).forEach(function(conn) {
+      conn.close();
+    });
+  };
+
+  return board;
+};
--- server.js
+++ server.js
@@ -1,16 +1,24 @@
 const express = require('express')
 const http = require('http')
-const RTCSwitchboard = require('rtc-switchboard')
+
+const RTCSwitchboard = require('./rtc-switchboard')
 
 const app = express()
 const server = http.Server(app)
 const switchboard = RTCSwitchboard(server)
 
-const PORT = parseInt(process.env.PORT) || 3000
+const PORT = parseInt(process.env.PORT) || 5000
 
 app.get('/', (req, res) => {
   res.send('hello friend')
 })
+
+switchboard.on('room:exit', function(payload) {
+  // inform each room member of the exit
+  payload.room.members.forEach(member => {
+    member.emit('data', `/peerleave|${payload.peer.id}`)
+  })
+});
 
 // start the server
 server.listen(PORT, (err) => {
