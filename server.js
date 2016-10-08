d99c9a9423054264439a0c2ebecc441f61073730
Thomas Peterson <t.ripley.peterson@gmail.com>
2016-10-08 02:57:35 -0400

    Initial commit

---
+++ server.js
@@ -0,0 +1,17 @@
+const express = require('express')
+const http = require('http')
+const RTCSwitchboard = require('rtc-switchboard')
+
+const app = express()
+const server = http.Server(app)
+
+const PORT = parseInt(process.env.PORT) || 3000
+
+// start the server
+server.listen(PORT, (err) => {
+  if (err) {
+    return console.log('Encountered error starting server: ', err)
+  }
+
+  console.log('Server running on port' + PORT)
+});
